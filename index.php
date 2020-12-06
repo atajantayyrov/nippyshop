37f5ce0aa6048dd07c618541f60dedab41855b61
atajan <atadzantajyrov020@gmail.com>
2020-12-06 19:12:37 +0300

    header

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nippyshop</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&family=Playfair+Display:wght@400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&family=Open+Sans:wght@300;400;600;700;800&family=Playfair+Display:wght@400;500;600;700;800;900&display=swap" rel="stylesheet">
+        <link
+      rel="stylesheet"
+      href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+    />
+    <link rel="stylesheet" href="css/style.css" />
+</head>
+<body>
+    
+<?php include 'web-parts/header.php'; ?> 
+    <main class="main">
+      <?php include 'web-parts/masthead.php'; ?>
+    </main>
+    <script src="js/front-page.js"></script>
+
+
+</body>
+</html>
---
+++ web-parts/header.php
@@ -0,0 +1,43 @@
+<header>
+    <div class="np-header">
+        <div class="np-container">
+            <div class="np-logo">
+                <a href="#!">
+                    Nippy
+                </a>
+            </div>
+
+            <nav class="np-nav">
+                <ul class="np-nav__list">
+                    <il class="np-nav__link">
+                    <a href="#!">ABOUT US</a>
+                    </il>
+                    <il class="np-nav__link">
+                    <a href="#!">SHOP</a>
+                    </il>
+                    <il class="np-nav__link">
+                    <a href="#!">PORTFOLIO</a>
+                    </il>
+                    <il class="np-nav__link">
+                    <a href="#!">BLOG</a>
+                    </il>
+                    <il class="np-nav__link">
+                    <a href="#!">ELEMENTS</a>
+                    </il>
+                </ul>
+            </nav>
+         </div>
+    </div>
+        <!-- <div class="np-container"> -->
+            <div class="np-content">
+                <div class="np-content__text">
+                    <h1>God is in the details</h1>
+                    <p>Vero nisi doloremque, at accusantium iure, error architecto dolor aperiam aspernatur modi corrupti minus in obcaecati qui.</p>
+                </div>
+                <div class="np-content__button">
+                    <a href="#!">LEARN MORE</a>
+                </div>
+            </div>
+        <!-- </div>           -->
+        
+</header>
